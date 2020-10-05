aaf11de3a2b089d855378f2bc6c022414da3cca5
Marcel Emonds <marcel.emonds@havasmedia.com>
2020-10-05 22:11:56 +0200

    added functionality to either play or save audiobook as a file

--- pdf_to_audiobook.py
+++ pdf_to_audiobook.py
@@ -2,26 +2,40 @@ import pyinputplus as pyip
 import os
 import sys
 import filetype
+import PyPDF2
+import pyttsx3
 
 
-def audiobook_maker(filepath, choice):
+def audiobook_maker(filepath, filename, choice):
+    pdf = open(filepath, 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(pdf)
+    num_pages = pdf_reader.numPages
+    player = pyttsx3.init()
     if choice == 'listen directly':
-        print('listen')
+        for num in range(0, num_pages):
+            page = pdf_reader.getPage(num)
+            data = page.extractText()
+            player.say(data)
+            player.runAndWait()
+        sys.exit()
+
     elif choice == 'as a file':
-        print('file')
+        for num in range(0, num_pages):
+            output = f'{filename}_part-{num+1}.mp3'
+            page = pdf_reader.getPage(num)
+            data = page.extractText()
+            player.save_to_file(data, output)
+            player.runAndWait()
 
 
 def pdf_to_audiobook():
     filepath = pyip.inputFilepath(prompt='Please enter the path to your pdf: ', mustExist=True, limit=2)
     file_type = filetype.guess(filepath).extension
-    print(file_type)
-    # print(file_type.mime)
     if file_type != 'pdf':
         sys.exit('Filetype is not supported. Please insert a path to a pdf file.')
-
+    filename = os.path.basename(filepath).split('.')[0].replace(' ','_')
     choice = pyip.inputMenu(['listen directly', 'as a file'], limit=2, numbered=True, prompt='How would you like your audiobook?')
-    print(choice)
-    audiobook_maker(filepath, choice)
+    audiobook_maker(filepath, filename, choice)
 
 
 if __name__ == '__main__':
